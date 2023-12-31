65f5537c4203ec71b9d22d8a7d67f46c0b0ab8c8
MTawhid7 <mtawhidulislam7@gmail.com>
2023-12-31 22:53:48 +0600

    Create virtual environment

--- PythonPlayground/main.py
+++ PythonPlayground/main.py
@@ -1,15 +1,3 @@
-def generate_fibonacci(limit):
-    fibonacci_sequence = []
-    a, b = 0, 1
-    while a < limit:
-        fibonacci_sequence.append(a)
-        a, b = b, a + b
-    return fibonacci_sequence
-
-
-# Generating Fibonacci sequence up to a limit of 100
-limit = 100
-fib_sequence = generate_fibonacci(limit)
-
-print(f"Fibonacci sequence up to {limit}:")
-print(fib_sequence)
+s = "\n"
+raw_string = repr(s)[1:-1]
+print(raw_string)
